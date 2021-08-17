67993e55eb5066045b16bb1ea06ea563b78b00a6
Kent Yucra <mckkentmwk@gmail.com>
2021-08-17 16:07:37 -0500

    first commit

---
+++ src/sales/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Sale
+# Create your views here.
+
+
+def home_view(request):
+    hello = "Hello world from the view"
+    return render(request, 'sales/home.html', {'hello': hello})
+
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
